7edc97a7af7f5e93d4114a27a87dd5d71844c273
mahesh <gangurdemahesh9@gmail.com>
2023-04-12 01:05:11 +0530

    [mahesh]add program file for CSV file Read and Write operation

---
+++ Day14_CSV_File_JSON_File/AddressBookRepo.cs
@@ -0,0 +1,56 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_CSV_File_JSON_File
+{
+    public class AddressBookRepo
+    {
+        public static void AddContact()
+        {
+            string filePath = @"E:\BridgeLabz\RFP 267\Day14_CSV_File_JSON_File\Day14_CSV_File_JSON_File\CSV_File\AddressBook1.csv";
+            AddressBook contact = new AddressBook();
+            StreamWriter streamWriter = new StreamWriter(filePath,append: true);
+            CsvWriter csvwriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter First name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address");
+            contact.Email = Console.ReadLine();
+
+            List<AddressBook> contacts = new List<AddressBook>();
+            contacts.Add(contact);
+            csvwriter.WriteRecords(contacts);
+            streamWriter.Flush();
+
+        }
+
+        public static void ReadDataFromCSVFile()
+        {
+            string filePath = @"E:\BridgeLabz\RFP 267\Day14_CSV_File_JSON_File\Day14_CSV_File_JSON_File\CSV_File\AddressBook1.csv";
+            StreamReader streamReader = new StreamReader(filePath);
+            CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            List<AddressBook> addressBooks = csvReader.GetRecords<AddressBook>().ToList();
+            
+                foreach (var contact in addressBooks)
+                {
+                    Console.WriteLine("FirstName " + contact.FirstName + "\nLastName " + contact.LastName + "\nAddress " + contact.Address + "\nCity " + contact.City + "\nZip " + contact.Zip + "\nPhoneNumber " + contact.PhoneNumber + "\nEmail " + contact.Email);
+                }
+            
+        }
+    }
+}
--- Day14_CSV_File_JSON_File/Program.cs
+++ Day14_CSV_File_JSON_File/Program.cs
@@ -11,6 +11,22 @@ namespace Day14_CSV_File_JSON_File
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 14 Practice Problem CSV file JSON file ");
+            Console.WriteLine("Select option for\n" +
+                "1. For Write data into CSV file\n" +
+                "2. For Read data from CSV file");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    AddressBookRepo.AddContact();
+                    break;
+                case 2:
+                    AddressBookRepo.ReadDataFromCSVFile();
+                    break;
+                default:
+                    Console.WriteLine("Select Correct Option");
+                    break;
+            }
         }
     }
 }
